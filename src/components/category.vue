a372bb73ce9207b2fcd3c449dae3239bcc0ed8a7
tickTock <18708383617@sina.cn>
2019-04-19 17:38:34 +0800

    slot demo

--- src/components/category.vue
+++ src/components/category.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="content">分类</div>
+  <div class="container">
+    <div class="content">分类</div>
+    <slotDemo>
+      <div slot="test">
+        <span>菜单1</span>
+        <span>菜单2</span>
+        <span>菜单3</span>
+        <span>菜单4</span>
+        <span>菜单5</span>
+        <span>菜单6</span>
+      </div>
+    </slotDemo>
+  </div>
 </template>
 
 <script>
-export default {
+import slotDemo from './slotDemo.vue'
 
+export default {
+  components: {
+    slotDemo
+  }
 }
 </script>
 
---
+++ src/components/slotDemo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content">
+    <div>slot内容如下</div>
+    <slot name="test"></slot>
+  </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
+
+<script>
+export default {
+
+}
+</script>
